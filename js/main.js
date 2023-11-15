a525275ae0626e6937d809af31918f62212afa77
NateA369 <abaten@uw.edu>
2023-11-14 20:44:39 -0800

    more updates

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibmF0ZWEzNjkiLCJhIjoiY2xvejhldmUxMDltdDMybXB6ZGNwdXh0MSJ9.dhamz50QtURIQ_BhPS78aw';
 
+
 let map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
-    zoom: 5.5, // starting zoom
-    center: [138, 38] // starting center
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    zoom: 5.5,
+    center: [138, 38]
 });
 
 let earthquakes, japan;
@@ -17,6 +18,8 @@ async function geojsonFetch(callback) {
         response = await fetch('assets/japan.json');
         japan = await response.json();
 
+        console.log('Data fetched:', earthquakes, japan);
+
         // Call the callback function once data is loaded
         callback();
     } catch (error) {
@@ -26,6 +29,8 @@ async function geojsonFetch(callback) {
 
 function loadingData() {
     map.on('load', function () {
+        console.log('Map loaded');
+
         map.addSource('earthquakes', {
             type: 'geojson',
             data: earthquakes
@@ -63,3 +68,56 @@ function loadingData() {
 // Pass the loadingData function as a callback to geojsonFetch
 geojsonFetch(loadingData);
 
+table = document.getElementsByTagName("table")[0];
+let row, cell1, cell2, cell3;
+for (let i = 0; i < earthquakes.features.length; i++) {
+    // Create an empty <tr> element and add it to the 1st position of the table:
+    row = table.insertRow(-1);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+    cell3 = row.insertCell(2);
+    cell1.innerHTML = earthquakes.features[i].properties.id;
+    cell2.innerHTML = earthquakes.features[i].properties.mag;
+    cell3.innerHTML = new Date(earthquakes.features[i].properties.time).toLocaleDateString(
+        "en-US");
+}
+let btn = document.getElementsByTagName("button")[0];
+
+btn.addEventListener('click', sortTable);
+
+
+// define the function to sort table
+function sortTable(e) {
+    let table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementsByTagName("table")[0];
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = parseFloat(rows[i].getElementsByTagName("td")[1].innerHTML);
+            y = parseFloat(rows[i + 1].getElementsByTagName("td")[1].innerHTML);
+            //check if the two rows should switch place:
+            if (x < y) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
